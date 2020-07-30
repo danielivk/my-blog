db87bdd84bcc0d33df6958d38f708e6ad87a46b4
Daniel Ivkovich <daniel.ivk.software@gmail.com>
2020-07-30 17:40:04 +0300

    First Commit

---
+++ src/build/precache-manifest.88a06b9f5c3c9414e4a763e60d131d65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4998471415bb2592a2d520d7951b81b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bdd0c339ac4d0d75c40",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "32d9b01aac01823fffaa",
+    "url": "/static/js/2.584bd2a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.584bd2a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bdd0c339ac4d0d75c40",
+    "url": "/static/js/main.2f67bd28.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
